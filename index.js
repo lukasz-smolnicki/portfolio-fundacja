6af9b3b4e42f7f2429fd556f249fffc293420b4e
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2020-02-27 22:33:59 +0100

    Fixed toggler side to left

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(window).scroll(function () {
-    let top = $(window).scrollTop();
-    let find_class_small = $.contains('mainNav', 'nav__scrolled');
+    let topScrolledWindow = $(window).scrollTop();
+    let classHandler = $.contains('mainNav', 'nav__scrolled');
 
-    if (top > 0 && find_class_small == false) {
+    if (topScrolledWindow > 0 && classHandler === false) {
         $('#mainNavbar').addClass('nav__scrolled-true');
         $('#mainNavbar').removeClass('nav__scrolled-false');
     } else {
